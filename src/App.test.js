60d3b08c90d8037e90d8e0656f32bdf82578db85
paulthechampion <enwerempaulo@gmail.com>
2022-10-31 08:26:55 -0400

    uncommenting test files and fixing ios date issues

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,12 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-// // test('renders learn react link', () => {
-// //   render(<App />);
-// //   const linkElement = screen.getByText(/learn react/i);
-// //   expect(linkElement).toBeInTheDocument();
-// // });
-// it('Render react App', () => {
-//   const div = document.createElement("div");
-//   render(<App />, div);
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
 // });
+it('Render react App', () => {
+  const div = document.createElement("div");
+  render(<App />, div);
+});
--- src/__test__/AddTransaction.test.js
+++ src/__test__/AddTransaction.test.js
@@ -1,16 +1,16 @@
-// import React from "react";
-// import { render, screen } from '@testing-library/react';
-// import AddTransactions from "../components/AddTransactions";
-// import transactionList from "../assets/transactionList";
+import React from "react";
+import { render, screen } from '@testing-library/react';
+import AddTransactions from "../components/AddTransactions";
+import transactionList from "../assets/transactionList";
 
 
 
-// const TransactionContext = React.createContext();
+const TransactionContext = React.createContext();
 
-// it("renders without crashing", () => {
-// 	const transactions = new transactionList()
-// 	const div = document.createElement("div");
-// 	render(<TransactionContext.Provider value={transactions}>
-// 				<AddTransactions></AddTransactions>
-// 			</TransactionContext.Provider>, div)
-// })
+it("renders without crashing", () => {
+	const transactions = new transactionList()
+	const div = document.createElement("div");
+	render(<TransactionContext.Provider value={transactions}>
+				<AddTransactions></AddTransactions>
+			</TransactionContext.Provider>, div)
+})
